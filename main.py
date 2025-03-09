60a8818bcb2f8ad246d8dabb09dad7672a743a33
Lucas Felipe Patera <lucasfelipepatera@gmail.com>
2025-03-09 15:31:22 -0300

    alteracoes_lucas

--- main.py
+++ main.py
@@ -1,7 +1,20 @@
 from cliente import Cliente
 from item import Item
+from pedido.pedido_retirada import PedidoRetirada
+from pedido.pedido_delivery import PedidoDelivery
+from pagamento.pagamento_cartao import PagamentoCartao
+from pagamento.pagamento_pix import PagamentoPIX
 
-cliente = Cliente("Laís", "Alura")
+cliente = Cliente("Lucas", "Sicredi")
 item_um = Item("Pizza", 30.0)
 item_dois = Item("Refrigerante", 5.0)
-print(f"Item: {item_um.nome}, Preço: {item_um.preco}")
+itens = [item_um, item_dois]
+
+taxa_entrega = 10.0
+pedido_retirada = PedidoRetirada(cliente, itens)
+pedido_delivery = PedidoDelivery(cliente, itens, taxa_entrega)
+
+valor_pedido =  pedido_delivery.calcular_total()
+pagamento_cartao = PagamentoCartao().processar(valor_pedido)
+pagamento_pix = PagamentoPIX()
+pagamento_pix.processar(valor_pedido)
